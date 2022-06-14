79b6a7cf3d8c5babd9ab3af82a348ae28c1dabee
George Wei <gzhihongwei@gmail.com>
2022-06-14 10:39:31 -0400

    Added CurrentAttributes to init

--- macaw/core/interaction_handler/__init__.py
+++ macaw/core/interaction_handler/__init__.py
@@ -1,5 +1,14 @@
 """
-FILE DESCRIPTION
+The interaction handler init
 
-Authors: Hamed Zamani (hazamani@microsoft.com)
+Authors: Hamed Zamani (hazamani@microsoft.com), George Wei (gzwei@umass.edu)
 """
+from injector import singleton
+
+from .msg import Message
+from .user_requests_db import InteractionDB
+
+
+@singleton
+class CurrentAttributes:
+    pass
