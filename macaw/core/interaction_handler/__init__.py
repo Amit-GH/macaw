714d006c66a3ea36cfda6ec401ec5ee0300170d0
George Wei <gzhihongwei@gmail.com>
2022-06-15 11:26:05 -0400

    Moved attributes to separate file

--- macaw/core/interaction_handler/__init__.py
+++ macaw/core/interaction_handler/__init__.py
@@ -1,14 +1,8 @@
 """
-The interaction handler init
+The interaction handler init.
 
 Authors: Hamed Zamani (hazamani@microsoft.com), George Wei (gzwei@umass.edu)
 """
-from injector import singleton
-
+from .attributes import CurrentAttributes, UserAttributes
 from .msg import Message
 from .user_requests_db import InteractionDB
-
-
-@singleton
-class CurrentAttributes:
-    pass
